5febf67aa4897d2c45a514e254db165c7181f6e8
Darius Jokilehto <dariusjokilehto+os@gmail.com>
2023-08-18 14:35:12 +0200

    Support display names with non-ISO-8859-1 characters
    The SAML plugin relied on request headers to pass the display name to
    Gerrit. This was problematic as Gerrit follows RFC-7230's recommendation
    [1] of using ISO-8859-1 characters - ideally US-ASCII only. This worked
    until change 94918 enforced ISO-8859-1 encoding in the display name
    header, and unintentionally (presumably, as there are no tests) also
    broke characters that are present but have different encodings in
    ISO-8859-1 and UTF-8 (a bug report was raised for this [2]).
    
    This change implements Luca's suggestion [3] not to use headers at all,
    but instead make a call via Gerrit's API once the user has been
    authenticated. This happens after login and circumvents the issue around
    header charsets.
    
    [1] https://datatracker.ietf.org/doc/html/rfc7230#section-3.2.4
    [2] https://issues.gerritcodereview.com/issues/296001714
    [3] https://gerrit-review.googlesource.com/c/plugins/saml/+/369676/comments/2da3a9f3_1f3c9af3
    
    Bug: Issue 40012596
    Change-Id: I321cb9d17d8013a042ea7a4822a606dde8329303

---
+++ src/test/java/com/googlesource/gerrit/plugins/saml/SamlWebFilterIT.java
@@ -0,0 +1,81 @@
+package com.googlesource.gerrit.plugins.saml;
+
+import static com.google.common.base.Strings.nullToEmpty;
+import static com.google.common.truth.Truth.assertThat;
+import static javax.servlet.http.HttpServletResponse.SC_OK;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+
+import com.google.gerrit.acceptance.AbstractDaemonTest;
+import com.google.gerrit.extensions.common.AccountDetailInfo;
+import com.google.gerrit.extensions.restapi.RestApiException;
+import com.google.gerrit.testing.ConfigSuite;
+import com.google.gerrit.util.http.testutil.FakeHttpServletRequest;
+import com.google.gerrit.util.http.testutil.FakeHttpServletResponse;
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import org.eclipse.jgit.errors.ConfigInvalidException;
+import org.eclipse.jgit.lib.Config;
+import org.junit.Test;
+
+public class SamlWebFilterIT extends AbstractDaemonTest {
+
+  @ConfigSuite.Default
+  public static Config setupSaml() throws ConfigInvalidException {
+    Config cfg = new Config();
+    cfg.fromText(
+        ""
+            + "[httpd]\n"
+            + "    filterClass = com.googlesource.gerrit.plugins.saml.SamlWebFilter\n"
+            + "[saml]\n"
+            + "    keystorePath = etc/samlKeystore.jks\n"
+            + "    metadataPath = http://localhost:8080/auth/realms/master/protocol/saml/descriptor\n"
+            + "[auth]\n"
+            + "    type = HTTP\n"
+            + "    httpHeader = X-SAML-UserName\n"
+            + "    httpDisplaynameHeader = X-SAML-DisplayName\n"
+            + "    httpEmailHeader = X-SAML-EmailHeader\n"
+            + "    httpExternalIdHeader = X-SAML-ExternalId");
+    return cfg;
+  }
+
+  @Test
+  public void supportAccountNamesWithNonIso88591Characters() throws IOException, ServletException, RestApiException {
+    SamlWebFilter samlWebFilter = server.getTestInjector().getInstance(SamlWebFilter.class);
+
+    String samlDisplayName = nullToEmpty(user.displayName()) + " Saml Test 合覺那加情力心";
+
+    HttpSession httpSession = mock(HttpSession.class);
+    AuthenticatedUser authenticatedUser =
+        new AuthenticatedUser(user.username(), samlDisplayName, user.email(), "externalId");
+    doReturn(authenticatedUser).when(httpSession).getAttribute(SamlWebFilter.SESSION_ATTR_USER);
+
+    FakeHttpServletRequest req = new FakeHttpServletRequestWithSession(httpSession);
+    req.setPathInfo(SamlWebFilter.GERRIT_LOGIN);
+    HttpServletResponse res = new FakeHttpServletResponse();
+
+    samlWebFilter.doFilter(req, res, mock(FilterChain.class));
+
+    assertThat(res.getStatus()).isEqualTo(SC_OK);
+
+    AccountDetailInfo account = gApi.accounts().id(user.username()).detail();
+    assertThat(account.name).isEqualTo(samlDisplayName);
+  }
+
+  private static class FakeHttpServletRequestWithSession extends FakeHttpServletRequest {
+    HttpSession session;
+
+    public FakeHttpServletRequestWithSession(HttpSession session) {
+      super();
+      this.session = session;
+    }
+
+    @Override
+    public HttpSession getSession() {
+      return session;
+    }
+  }
+}
